254c19c6f8770a2c88dfb47481c9bc72d85f071a
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-02-06 10:47:58 +0100

    complex state 1.

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 function Counter() {
     const [count, setCount] = useState(0)
+    const [color, setColor] = useState('#DC0073')
 
     return (
         <div>
-            Current count is: {count}
+            <span style={{ color }}>Current count is: {count}</span>
             <br />
             <button onClick={() => setCount(count + 1)}>Increase count</button>
+            <button onClick={() => setColor(color === '#DC0073' ? '#00A1E4' : '#DC0073')}>
+                Change Color
+            </button>
         </div>
-    );
-};
+    )
+}
 
-export default Counter;
+export default Counter
