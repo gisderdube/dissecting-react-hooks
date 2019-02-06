4ad20610f4baa2398e485428b43c59a652e682d5
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-02-06 14:36:09 +0100

    event listener hook

--- src/Application.js
+++ src/Application.js
@@ -4,7 +4,14 @@ import Counter from './Counter'
 
 function Application() {
     return (
-        <div>
+        <div
+            style={{
+                minHeight: '150vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+            }}
+        >
             <h1>Making sense of React Hooks</h1>
             <Counter />
         </div>
--- src/Counter.js
+++ src/Counter.js
@@ -1,37 +1,18 @@
 import React, { useEffect, useState, useRef } from 'react'
 
-function useInterval(callback, delay) {
-    const savedCallback = useRef()
-
-    useEffect(() => {
-        savedCallback.current = callback
-    })
-
-    useEffect(() => {
-        function tick() {
-            savedCallback.current()
-        }
-        if (delay !== null) {
-            let id = setInterval(tick, delay)
-            return () => clearInterval(id)
-        }
-    }, [delay])
-}
-
 function Counter() {
     const [count, setCount] = useState(0)
 
-    useInterval(() => {
-        setCount(count + 1)
-    }, 1000)
+    function scrollListener() {
+        setCount(window.pageYOffset)
+    }
+    useEffect(() => {
+        document.addEventListener('scroll', scrollListener)
 
-    return (
-        <div>
-            Current count is: {count}
-            <br />
-            <button onClick={() => setCount(count + 1)}>Increase count</button>
-        </div>
-    )
+        return () => document.removeEventListener('scroll', scrollListener)
+    }, [])
+
+    return <div>Current count is: {count}</div>
 }
 
 export default Counter
