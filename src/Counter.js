4f712f38f5c47267750ec78dec92398cffeaaf1d
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-02-06 11:11:48 +0100

    complex state 3.

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
+
+function counterReducer(state, action) {
+    if (action.type === 'increaseCount') return { ...state, count: state.count + 1 }
+    if (action.type === 'toggleColor')
+        return { ...state, color: state.color === '#DC0073' ? '#00A1E4' : '#DC0073' }
+    return state
+}
 
 function Counter() {
-    const [state, setState] = useState({
+    const [state, dispatch] = useReducer(counterReducer, {
         count: 0,
         color: '#DC0073',
     })
@@ -12,14 +19,8 @@ function Counter() {
         <div>
             <span style={{ color }}>Current count is: {count}</span>
             <br />
-            <button onClick={() => setState({ ...state, count: count + 1 })}>Increase count</button>
-            <button
-                onClick={() =>
-                    setState({ ...state, color: color === '#DC0073' ? '#00A1E4' : '#DC0073' })
-                }
-            >
-                Change Color
-            </button>
+            <button onClick={() => dispatch({ type: 'increaseCount' })}>Increase count</button>
+            <button onClick={() => dispatch({ type: 'toggleColor' })}>Change Color</button>
         </div>
     )
 }
